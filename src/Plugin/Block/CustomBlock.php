40b439905e687c4648e689de57c23adc13078444
grgwrd <mailgregcarlson@gmail.com>
2022-08-19 08:40:21 -0500

    added custom block

---
+++ src/Plugin/Block/CustomBlock.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\custom_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'MightAlsoLikeBlock' block.
+ *
+ * @Block(
+ *  id = "custom_block",
+ *  admin_label = @Translation("Custom Starter block""),
+ * )
+ */
+  class CustomBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $text = [ '#markup' => $this->t('Test') ];
+
+    $build = [
+      '#theme' => 'custom_block',
+      '#related_products' => $text
+    ];
+
+    return $build;
+  }
+
+}
